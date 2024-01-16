b3b269ca1a188534a84f7cac6fdebafa148f6c6e
issam-nz <113427473+issam-nz@users.noreply.github.com>
2024-01-16 21:29:25 +0100

    Create productos-data.js

---
+++ productos-data.js
@@ -0,0 +1,10 @@
+const productosData = [
+    { "id": 1, "nombre": "Producto 1", "precio": 20.0 },
+    { "id": 2, "nombre": "Producto 2", "precio": 30.0 },
+    { "id": 3, "nombre": "Producto 3", "precio": 25.0 },
+    { "id": 3, "nombre": "Producto 4", "precio": 33.0 },
+    { "id": 4, "nombre": "Producto 5", "precio": 32.0 },
+    { "id": 5, "nombre": "Producto 6", "precio": 31.0 }
+];
+
+const carrito = [];
