8df5f76b21bb7014c404defb656df31324baf21a
issam-nz <113427473+issam-nz@users.noreply.github.com>
2024-01-23 22:16:56 +0100

    Update productos-data.js

--- productos-data.js
+++ productos-data.js
@@ -1,10 +1,16 @@
 const productosData = [
-    { "id": 1, "nombre": "Producto 1", "precio": 20.0 },
-    { "id": 2, "nombre": "Producto 2", "precio": 30.0 },
-    { "id": 3, "nombre": "Producto 3", "precio": 25.0 },
-    { "id": 3, "nombre": "Producto 4", "precio": 33.0 },
-    { "id": 4, "nombre": "Producto 5", "precio": 32.0 },
-    { "id": 5, "nombre": "Producto 6", "precio": 31.0 }
+    {"id": 131673, "nombre": "Double Cut Guitar", "precio": 799.99},
+    {"id": 147148, "nombre": "Single Cut Guitar", "precio": 899.99},
+    {"id": 283849, "nombre": "Heavy Guitar", "precio": 1299.99},
+    {"id": 349014, "nombre": "Jazz Guitar", "precio": 1099.99},
+    {"id": 362632, "nombre": "Lefthanded Guitar", "precio": 849.99},
+    {"id": 372488, "nombre": "ST Style Gui­tar", "precio": 1199.99},
+    {"id": 378249, "nombre": "Premium Guitar", "precio": 1499.99},
+    {"id": 379208, "nombre": "7 String Guitar", "precio": 999.99},
+    {"id": 397231, "nombre": "Fanfret Guitar", "precio": 1599.99},
+    {"id": 402463, "nombre": "T Style Guitar", "precio": 1099.99},
+    {"id": 406187, "nombre": "Semiacoustic Guitar", "precio": 1299.99},
+    {"id": 415885, "nombre": "8 String Guitar", "precio": 1399.99}
 ];
 
 const carrito = [];
